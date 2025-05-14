f1a0dda5686e6b7c618321b0ae59f7be1729a888
ymohit1603 <ymohit1603@gmail.com>
2025-05-14 09:56:37 +0530

    chore: remove unused public assets and textures

--- public/textures/grass.js
+++
@@ -1,5 +0,0 @@
-
-// This is a JavaScript module that exports the path to a grass texture
-// so we can access it from our code
-
-export const grassTextureBase64 = "/textures/grass.jpg";
--- public/textures/stone.js
+++
@@ -1,5 +0,0 @@
-
-// This is a JavaScript module that exports the path to a stone texture
-// so we can access it from our code
-
-export const stoneTextureBase64 = "/textures/stone.jpg";
